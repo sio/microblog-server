493138cbf3531d0fb3e8286275755be0f35f5a3c
Vitaly Potyarkin <sio.wtf@gmail.com>
2022-07-04 17:53:01 +0300

    Add workaround for tox bug on Windows

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 from tempfile import TemporaryDirectory
 from pathlib import Path
+import os
+import sys
 
 import pytest
 
@@ -10,3 +12,8 @@ def repo():
     with TemporaryDirectory(prefix='microblog_test_') as temp:
         repo = git.Repo.init(temp)
         yield Path(temp)
+
+
+# Tox clears USERNAME on Windows leading to errors in getpass.getuser()
+if sys.platform == 'win32' and 'USERNAME' not in os.environ:
+    os.environ['USERNAME'] = 'microblog-test'
