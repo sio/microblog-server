e28781d47c3697b56d360fc1852d716c2739d7b8
Vitaly Potyarkin <sio.wtf@gmail.com>
2022-07-21 21:12:45 +0300

    Change default log level to INFO

--- src/microblog/logging.py
+++ src/microblog/logging.py
@@ -1,7 +1,8 @@
 import logging
+import os
 
 log = logging.getLogger(__package__)
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.DEBUG if 'DEBUG' in os.environ else logging.INFO,
     format='%(levelname)-8s %(message)s',
 )
