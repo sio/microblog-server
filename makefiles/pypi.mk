eba6876344b8ec95659e958a5b43b08cdde0f7a6
Vitaly Potyarkin <sio.wtf@gmail.com>
2022-07-27 10:11:11 +0300

    Show which packages we're about to upload to PyPI

--- makefiles/pypi.mk
+++ makefiles/pypi.mk
@@ -7,5 +7,6 @@ dist: | venv $(VENV)/build
 
 .PHONY: upload
 upload: dist | $(VENV)/twine
+	ls dist/*
 	$(VENV)/twine upload --repository testpypi $(TWINE_ARGS) dist/*
 	$(VENV)/twine upload $(TWINE_ARGS) dist/*
